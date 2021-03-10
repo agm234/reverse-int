9a8e336c4912a6cec8528cab221d34e1a94553d8
agm234 <79218134+agm234@users.noreply.github.com>
2021-03-10 16:11:50 +0300

    feat: add reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    let text = '';
+    let text1 = String(n).split('');
+    text1.reverse();
+    for (let i = 0; i < text1.length; i++) {
+        text = text + text1[i];
+    }
+    return Number(text);
+
 }
